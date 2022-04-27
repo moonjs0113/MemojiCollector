b9f02ff1f3612c69f0b903fa90f8c9bc9bdfbc27
moonjs0113 <littlemoom@naver.com>
2022-04-27 14:51:13 +0900

    Create Project

---
+++ MemojiCollector/MemojiCollector/MemojiCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemojiCollectorApp.swift
+//  MemojiCollector
+//
+//  Created by Moon Jongseek on 2022/04/27.
+//
+
+import SwiftUI
+
+@main
+struct MemojiCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
