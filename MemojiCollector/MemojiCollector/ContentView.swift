596567c301afddb2cd09bc5f33d78f8b675712d2
moonjs0113 <littlemoom@naver.com>
2022-04-27 15:34:52 +0900

    Add Main UI

--- MemojiCollector/MemojiCollector/ContentView.swift
+++ MemojiCollector/MemojiCollector/ContentView.swift
@@ -8,9 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var sessionArray = ["All", "Morning", "Afternoon"]
+    private var gridItems = [GridItem(.flexible()), GridItem(.flexible())]
+    @State private var session = "All"
+    @State private var searchText = ""
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            VStack {
+                Picker("",selection: self.$session) {
+                    ForEach(self.sessionArray, id: \.self) { session in
+                        Text(session)
+                    }
+                }
+                .pickerStyle(.segmented)
+                .padding(.horizontal)
+                ScrollView {
+                    LazyVGrid(columns: self.gridItems){
+                        ForEach(0..<100, id: \.self) { number in
+                            Text("Grid \(number)")
+                        }
+                    }
+                }
+                Spacer()
+            }
+            .navigationTitle("Memoji Collector")
+        }
+        .searchable(text: self.$searchText)
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
--- MemojiCollector/MemojiCollector/MemojiCollectorApp.swift
+++ MemojiCollector/MemojiCollector/MemojiCollectorApp.swift
@@ -6,12 +6,19 @@
 //
 
 import SwiftUI
+import Firebase
 
 @main
 struct MemojiCollectorApp: App {
+    
+    init() {
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(.light)
         }
     }
 }
